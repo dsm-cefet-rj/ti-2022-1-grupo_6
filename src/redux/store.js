caeea8c3ee8f5f5b2e6c709fa6ae707d7bdf9cd4
GabRodVal <lotorrpg@gmail.com>
2022-06-07 00:11:02 -0300

    Mudanças aos Favoritos

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 // configureStore setups redux devtools, adds thunk middleware, calls createStore
 import counterReducer from './features/counterSlice';
+import favoriteReducer from'./features/favoriteSlice';
 import { productsApi } from './features/productsApiSlice';
 
 export const store = configureStore({
   // automatically uses combineReducers
   reducer: {
     counter: counterReducer,
+    favorite: favoriteReducer,
     [productsApi.reducerPath]: productsApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
