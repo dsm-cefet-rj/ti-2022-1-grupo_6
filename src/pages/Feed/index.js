a080018cdbec7135270b968da0be36f5b5867d6f
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-06-08 02:06:56 -0300

    counter button removed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,29 +1,13 @@
 import { Carousel } from '../../components/Carousel';
 import { ProductCard } from '../../components/ProductCard';
-import { useDispatch, useSelector } from 'react-redux';
 import { useFetchProductsQuery } from '../../redux/features/productsApiSlice';
-import { amountAdded } from '../../redux/features/counterSlice';
 
 export const Feed = () => {
   const { data: products = [], isFetching } = useFetchProductsQuery();
 
-  const counter = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-
-  function handleClick() {
-    dispatch(amountAdded(3));
-    // localStorage.setItem('token.example', '123456789');
-  }
-
   return (
     <>
       <Carousel />
-
-      <div className="container my-5">
-        <button onClick={handleClick} className="btn btn-danger">
-          <p>Counter: {counter}</p>
-        </button>
-      </div>
 
       <div className="container my-5">
         <div className="row">
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 // configureStore setups redux devtools, adds thunk middleware, calls createStore
-import counterReducer from './features/counterSlice';
 import cartReducer from './features/cartSlice';
 import favoriteReducer from'./features/favoriteSlice';
 import { productsApi } from './features/productsApiSlice';
@@ -8,7 +7,6 @@ import { productsApi } from './features/productsApiSlice';
 export const store = configureStore({
   // automatically uses combineReducers
   reducer: {
-    counter: counterReducer,
     cart: cartReducer,
     favorite: favoriteReducer,
     [productsApi.reducerPath]: productsApi.reducer,
