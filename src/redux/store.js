192e19bc916f408858dc651a3b952faaf49ce67f
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-06-08 02:08:09 -0300

    removed some unused comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-// configureStore setups redux devtools, adds thunk middleware, calls createStore
 import cartReducer from './features/cartSlice';
 import favoriteReducer from'./features/favoriteSlice';
 import { productsApi } from './features/productsApiSlice';
 
 export const store = configureStore({
-  // automatically uses combineReducers
   reducer: {
     cart: cartReducer,
     favorite: favoriteReducer,
     [productsApi.reducerPath]: productsApi.reducer,
   },
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
-  // things that we have with SWR from Nextjs and React-query
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productsApi.middleware),
 });
