a43f36b956ca3cd4c748f75abb9ee57a4a3bde61
Thiago <thiagoandre2121@gmail.com>
2022-06-17 21:16:14 -0300

    added initial mercado pago checkout configuration

--- api/src/controllers/paymentsController.js
+++ api/src/controllers/paymentsController.js
@@ -1,8 +1,40 @@
 const MercadoPago = require('mercadopago');
+require('dotenv').config()
+const { v4 } = require("uuid");
 
+const getFullUrl = (req) => {
+    const url = req.protocol + '://' + req.get('host')
+    console.log(url)
+    return url
+  }
 
 module.exports = {
     async checkout(req, res) {
-        return 'teste'
-    }
+        MercadoPago.configure({
+            sandbox: true,
+            access_token: process.env.MP_ACCESS_TOKEN
+        })
+
+        const { items, payer } = req.body
+        const id = v4()
+
+        const purchaseOrder = {
+            items,
+            payer,
+            auto_return: 'all',
+            external_reference: id,
+            back_urls: {
+                success: getFullUrl(req) + '/payments/success',
+                pending: getFullUrl(req) + '/payments/pending',
+                failure: getFullUrl(req) + '/payments/failure',
+            }
+        }
+
+        try {
+            const preference = await MercadoPago.preferences.create(purchaseOrder)
+            return res.redirect(`${preference.body.init_point}`)
+        } catch (err) {
+            return res.send(err.message)
+        }
+    },
 }
