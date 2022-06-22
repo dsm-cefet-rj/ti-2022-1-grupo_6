4ae9f5d03ecb43965d9cc7e7ac8a164d858e36e0
GabRodVal <lotorrpg@gmail.com>
2022-06-22 09:38:09 -0300

    Add Wishlist Controller

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,9 +4,11 @@ const routes = Router();
 const paymentsController = require('../controllers/paymentsController');
 const { productsRoutes } = require('./products.routes');
 const { cartRoutes } = require('./cart.routes');
+const { wishlistRoutes } = require("./wishlist.routes");
 
 routes.post('/checkout', paymentsController.checkout);
 routes.use('/products', productsRoutes);
 routes.use('/cart', cartRoutes);
+routes.use("/wishlist", wishlistRoutes);
 
 module.exports = routes;
