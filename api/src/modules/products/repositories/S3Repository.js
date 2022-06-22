baa373c8fdf38c1dac3310f196428a318385c544
Felipe Curcio <felipe_nieto010@hotmail.com>
2022-06-22 10:50:06 -0300

    S3Repository - refactory

--- api/src/modules/products/repositories/S3Repository.js
+++ api/src/modules/products/repositories/S3Repository.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const mime = require('mime-types');
 
 class S3Repository {
+  constructor() {
+    this.s3Client = new S3Client({ region: 'us-east-1' });
+  }
+
   async uploadProductImage(filename) {
     const file = path.join(__dirname, '..', '..', '..', '..', 'tmp', filename);
     const fileStream = fs.createReadStream(file);
@@ -18,12 +22,8 @@ class S3Repository {
       ContentType: contentType,
     };
 
-    const REGION = 'us-east-1';
-
-    const s3Client = new S3Client({ region: REGION });
-
     try {
-      await s3Client.send(new PutObjectCommand(uploadParams));
+      await this.s3Client.send(new PutObjectCommand(uploadParams));
       fs.promises.unlink(file);
       const imageUrl = `https://${uploadParams.Bucket}.s3.amazonaws.com/${uploadParams.Key}`;
 
