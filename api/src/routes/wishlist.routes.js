78562144b82d3385d9399f5f3fc1ff6f4655911e
GabRodVal <lotorrpg@gmail.com>
2022-06-29 17:17:47 -0300

    WishlistController

--- api/src/routes/wishlist.routes.js
+++ api/src/routes/wishlist.routes.js
@@ -8,7 +8,7 @@ wishlistRoutes.get("/:wishlistId", wishlistController.getWishlist);
 wishlistRoutes.get("/", wishlistController.getAllWishlists);
 wishlistRoutes.post("/", wishlistController.createWishlist);
 wishlistRoutes.put("/:wishlistId", wishlistController.createFavorite);
-wishlistRoutes.put("/:wishlistId", wishlistController.removeFavorite);
+//wishlistRoutes.put("/:wishlistId", wishlistController.removeFavorite);
 wishlistRoutes.delete("/:wishlistId", wishlistController.deleteWishlist);
 
 
