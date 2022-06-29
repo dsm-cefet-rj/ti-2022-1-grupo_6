95a3ec36b5e5fdaebf23a56afe50952d1ac9f1bc
Felipe Curcio <felipe_nieto010@hotmail.com>
2022-06-29 04:20:06 -0300

    Mongoose get started

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -4,18 +4,11 @@ const connectDB = async () => {
   try {
     await mongoose.connect(
       `mongodb+srv://psw:${process.env.ATLAS_PASSWORD}@cluster0.tbhn8.mongodb.net/techbuyretry`
-      // {
-      //   useNewUrlParser: true,
-      //   useCreateIndex: true,
-      //   useFindAndModify: false,
-      //   useUnifiedTopology: true,
-      // }
     );
 
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    // Exit process with failure
     process.exit(1);
   }
 };
