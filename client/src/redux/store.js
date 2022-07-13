1e7e09c5d9dddc1cbd6dbe19dfeb47e9163bcaa4
GabRodVal <lotorrpg@gmail.com>
2022-07-13 00:20:30 -0300

    Wishlist - front-end

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -18,6 +18,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       productsApi.middleware,
-      userApiSlice.middleware
+      userApiSlice.middleware,
+      wishlistApi.middleware
     ),
 });
