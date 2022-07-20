ab2eca767d4690c86af8f8f6c2a60752eae271a4
Thiago <thiagoandre2121@gmail.com>
2022-07-19 23:13:32 -0300

    order page initial template

--- client/src/pages/Orders/index.js
+++ client/src/pages/Orders/index.js
@@ -21,18 +21,60 @@ export function Order() {
       `${process.env.REACT_APP_BASE_URL}/cart`,
       { headers: { Authorization: `Bearer ${token}` } }
     ))).data
-    
-    dispatch(updateCart({cart}));
+
+    dispatch(updateCart({ cart }));
   }
 
- 
-
-
   return (
-      <div>
-          pedidos
+    (
+      <div className="container">
+        <div className="product line">
+          <img
+            src={"https://felipecurciopsw.s3.amazonaws.com/images/image-1656909040928-78rtx090.jpg"}
+            alt="imagem do produto"
+            className="product-image"
+          />
+
+          <div className="product-info">
+            <span>teste</span>
+            <strong>{100}</strong>
+            <p>
+              2022-04-04
+            </p>
+
+            <div className="quant">
+              <p>Quant:</p>
+
+              <select
+                className="form-select bg-transparent"
+                id="quant"
+                onChange={(e) => { }}
+                defaultValue={100}
+                required
+              >
+                {Array.from({ length: 4 }, (el, i) => i + 1).map((element, idx) => (
+                  <option
+                    key={idx}
+                  >
+                    {element}
+                  </option>
+                ))}
+              </select>
+            </div>
+           
+          </div>
+          <div>
+              
+            </div>
+            <button onClick={() => {
+              window.location.href = "https://api.whatsapp.com/send?phone=5521995709363&text=Ol%C3%A1%2C%20vi%20seu%20an%C3%BAncio%20na%20TechBuy%2C%20gostaria%20de%20oferecer%20uma%20proposta!"
+            }}>
+              Contatar vendedor
+            </button>
+        </div>
       </div>
+    )
   )
-      
-  
+
+
 }
